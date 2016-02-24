68b39cee0366425088d5d59c970e5358ae6bacd2
Joel Harris <joelharris229@gmail.com>
2016-02-23 21:48:51 -0800

    set up calculator project. Tests are set up and referecing with intial test passing

---
+++ simple-calculator.tests/ExpressionTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace simple_calculator.tests
+{
+    [TestClass]
+    public class ExpressionTests
+    {
+        [TestMethod]
+        public void EnsureICanCallNewClass()
+        {
+            Expression math_add = new Expression();
+            Assert.IsNotNull(math_add);
+        }
+    }
+}
