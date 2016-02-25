3ee00cfdab995011926ac6a1f8681ba87d48c60d
Joel Harris <joelharris229@gmail.com>
2016-02-24 19:26:38 -0800

    started test on saving terms

--- simple-calculator.tests/ExpressionTests.cs
+++ simple-calculator.tests/ExpressionTests.cs
@@ -12,5 +12,28 @@ namespace simple_calculator.tests
             Expression math_add = new Expression();
             Assert.IsNotNull(math_add);
         }
+        [TestMethod]
+        public void EnsureEnteredExpressionisString()
+        {
+            Expression math_add = new Expression();
+            string exp = math_add.RecieveEquation();
+            Assert.AreEqual(typeof(string), exp.GetType());
+        }
+        [TestMethod]
+        public void EnsureNoSpacesPresentInExpression()
+        {
+            Expression math_add = new Expression();
+            string exp = math_add.RecieveEquation();
+            string expected = "1+2";
+            Assert.AreEqual(expected, exp);
+        }
+        [TestMethod]
+        public void EnsureCapturingTermsInExpression()
+        {
+            Expression math_add = new Expression();
+            string exp = math_add.collectTerms();
+
+        }
+
     }
 }
--- simple-calculator/Expression.cs
+++ simple-calculator/Expression.cs
@@ -8,6 +8,22 @@ namespace simple_calculator
 {
    public class Expression
     {
+        virtual public string RecieveEquation()
+        {
+            string equation = "1 + 2";
+            equation = equation.Replace(" ", "");
+            return equation;
+        }
 
+        public string collectTerms()
+        {
+            string equation = "1+2";
+            int operatorIndex = equation.IndexOfAny(new char[] { '+', '-', '/', '*' });
+            char op = equation[operatorIndex];
+            string[] terms = equation.Split(op);
+
+            return terms;
+
+        }
     }
 }
