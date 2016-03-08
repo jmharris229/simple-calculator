2818ee5c5215b1d082a60c26a234fe6a49a1ca01
jmharris229 <joelharris229@gmail.com>
2016-03-08 11:32:05 -0600

    refactored to account for negative, on initial look, all tests pass

--- simple-calculator/Expression.cs
+++ simple-calculator/Expression.cs
@@ -20,28 +20,68 @@ namespace simple_calculator
             Console.WriteLine("Write expression");
             string input = exp;          
             input = input.Replace(" ", "");
-            int operatorIndex = input.IndexOfAny(new char[] { '+', '-', '/', '*' },1);
+            char[] opArray = new char[] { '+', '-', '/', '*', '%' };
+            char ForP = '+';
 
-            if(operatorIndex == -1)
+            //check to to see if the 0 index is a negative. if so makes substring of input without it.
+            if (input[0] == '-')
+            {
+                ForP = '-';
+                
+                //input is now a string without the negative at the beginning.
+                input = input.Substring(1, input.Length);
+            }
+
+            //find the index of the operator
+            int operatorIndex = input.IndexOfAny(opArray, 1);
+
+            //throw an exception if no operator found
+            if (operatorIndex == -1)
             {
                 throw new NoOperatorException("Need an operator!");
             }
 
-            char op = input[operatorIndex];
-
-            //check to to see if the 0 index is a negative. if so makes substring of 
-
-            string[] terms = input.Split(new char[] { op }, 2);
-           
-            if(terms.Length != 2 || terms[1].Equals("") || terms[0].Equals(""))
+            char[] NoMinusOpArray = new char[] { '+', '/', '*', '%' };
+            foreach (var item in NoMinusOpArray)
             {
-                throw new InvalidTermException("Not right terms.");
+                if (input[0] == item || input[operatorIndex] + 1 == item || input[input.Length - 1] == item || input[input.Length - 1] == '-')
+                {
+                    throw new InvalidTermException("Invalid Term Placement.");
+                }
             }
 
+            char op = input[operatorIndex];
+            string[] terms = input.Split(new char[] { op }, 2);
+            /*//saves the index of the operator
+            
+
+            //will attempt to split the input into two strings based on new input which starts without the negative at the beginning if there was one. and should split on the first instance of the operator.
+            
+            
+            //checks to see if the length of terms is 2, or if either position is an empty string. This makes sure we have the right amount of terms.
+            if(terms.Length != 2 || terms[1].Equals("") || terms[0].Equals(""))
+            {
+                throw new InvalidTermException("Not right amount of terms.");
+            }*/
+
+
             ParsedExp expression = new ParsedExp();
-            expression.oper = op;
-            expression.term1 = int.Parse(input.Substring(0,operatorIndex-1));
-            expression.term2 = int.Parse(input.Substring(operatorIndex+1,input.Length-1));
+            if (ForP == '-')
+            {
+                expression.oper = op;
+                expression.term1 = int.Parse(ForP + terms[0]);
+                expression.term2 = int.Parse(terms[1]);
+            }
+            else
+            {
+                expression.oper = op;
+                expression.term1 = int.Parse(terms[0]);
+                expression.term2 = int.Parse(terms[1]);
+            }
+            
+            
+            /*expression.term1 = int.Parse(input.Substring(0,operatorIndex-1));
+            expression.term2 = int.Parse(input.Substring(operatorIndex+1,input.Length-1));*/
             return expression;
         }
     }
