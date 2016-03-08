6b2508ccfcee9996afd5ff0fe1a81e26c2b55387
jmharris229 <joelharris229@gmail.com>
2016-03-08 15:57:52 -0600

    added in evaluate tests

---
+++ simple-calculator.tests/evaluateTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace simple_calculator.tests
+{
+    [TestClass]
+    public class EvaluateTests
+    {
+        [TestMethod]
+        public void EnsureCalculateCanBeCalledCorrectly()
+        {
+            Evaluate mathey = new Evaluate();
+            int exp_result = 3;
+            int act_result = mathey.calculate("1+2");
+
+        }
+    }
+}
